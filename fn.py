577c201c2b897116000e60270507527323a2a15f
shivasubramani92 <shivavvcet32@gmail.com>
2018-08-30 12:29:17 +0530

    functions

---
+++ fn.py
@@ -0,0 +1,11 @@
+def changeme ( mylist ):
+    #mylist = [1,2,3,4,5];
+    print "values inside the function:",mylist
+    return mylist
+
+
+
+
+mylist = [10,20,30,40,50]
+result=changeme( mylist )
+print "values outside the function:",result
